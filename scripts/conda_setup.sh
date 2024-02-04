58d35d5c01ef3329e1e4e202367a383f983947cc
Corey Cheung <coreycheung@outlook.com>
2024-02-04 15:10:17 +1000

    conda set up

---
+++ scripts/conda_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+echo "Setting up nba-basketball-analytics conda env"
+
+# Config shell session to work with conda
+. ~/miniconda3/etc/profile.d/conda.sh
+
+conda deactivate
+conda env remove -n nba-basketball-analytics
+conda create -yqn nba-basketball-analytics python=3.10
+conda activate nba-basketball-analytics
+conda install \
+	--channel conda-forge \
+	--channel defaults \
+	--quiet \
+	--yes \
+    streamlit \
+	python-duckdb \
+	pandas \
+    numpy \
+    matplotlib \
+
+
+echo "Finished, now spin up your new conda environment with 'conda activate nba-basketball-analytics'"
